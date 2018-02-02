428a12dd165521906786e7199385e8a34e717851
juhyun0 <36025326+juhyun0@users.noreply.github.com>
2018-02-02 18:45:23 +0900

    Add files via upload
    숫자, 문자, 메소드

---
+++ text.py
@@ -0,0 +1,22 @@
+hello='Hello'
+world='World'
+s='Good Morning'
+
+hello_world=hello+', '+world #문자 결합
+print(hello_world)
+
+print(s[5:12]) #문자열 s의 5~12까지
+
+print(hello+s[5:12])
+
+print("")
+
+a='Good Morning'
+print(a[:4]) # = a[0:4]
+print(a[5:]) # = 5에서 끝까지
+
+print("")
+
+#특정위치에 있는 문자 참조
+print(a[0])
+print(a[8])
